1342af245a0083c2681b77b8b8e9816e57d45664
stefanos-larkou <s.larkou@outlook.com>
2026-08-18 15:20:38 +0300

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.scss";
+import "./styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
