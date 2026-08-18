41a2da27f7f000af9c898b79c5fc56b28b4c31fd
stefanos-larkou <s.larkou@outlook.com>
2026-08-18 16:07:09 +0300

    remove bootstrap and add mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./core/theme";
 
 const root = document.getElementById("root");
 if (!root) {
@@ -11,6 +16,11 @@ if (!root) {
 
 createRoot(root).render(
     <StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 );
